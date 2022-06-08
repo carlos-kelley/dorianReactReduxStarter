2a3dc4814aa8acd9f840280aee938d2428f62899
Dev Jana <devjana@Devs-MBP-2.hsd1.mn.comcast.net>
2022-06-08 18:14:59 -0500

    store provided

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// step 0 - require stuff from redux, react-redux
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'; 
+// step 1 - declare reducers
+const testReducer = ( state='hello world', action ) =>{
+    console.log( 'in testReducer:', action );
+    return state;
+} // end reducer (it is just a function)
+// step 2 - create store & combine reducers
+const store = createStore(
+    combineReducers({
+        testReducer
+    })
+) // end create store
+// step 3 - use provider to allow app to use store
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
